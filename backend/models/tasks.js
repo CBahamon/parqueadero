87fe967c12286dc2dc923790e80e49aefa5c85b5
Cristian Bahamon <bahamon2803@gmail.com>
2022-05-31 01:59:16 -0500

    add new routes

---
+++ backend/models/tasks.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ToRegister = new Schema({
+    userName: {type: String, required: true},
+    userId: {type: String, required: true},
+    vehicleNumber: {type:String, required: true},
+    vehicleType: {type:String, required: true},
+    vehicleDescription: {type:String, required: true},
+    checkInDateTime: {type: Date, required: false},
+    checkOutDateTime: {type:Date, required: false},
+    havePlace: {type: Boolean, required: true},
+    price: {type: Number, required: false},
+    status: {type: Boolean, required: false}
+
+})
+
+module.exports = mongoose.model('Records', ToRegister);
+
+
